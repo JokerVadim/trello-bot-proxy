cb1fc4b8c9c69b403a8f36ce8a9997d3d6b7677a
JokerVadim <91554339+JokerVadim@users.noreply.github.com>
2025-11-04 23:54:22 +0500

    Delete api/webhook.js

--- api/webhook.js
+++
@@ -1,8 +0,0 @@
-// NEW webhook.js - ultra simple
-export default function handler(req, res) {
-  console.log('Webhook called');
-  res.status(200).json({ 
-    message: 'NEW webhook is working!',
-    simple: true
-  });
-}
